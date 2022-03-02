2f4ca16f789f76a20bd6b8fdf48703c6cc48ea5f
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-03-02 10:50:54 +0100

    add content to about page

---
+++ components/Styles/Pages/About.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledAbout = styled.main`
+    padding: 0 2rem 2rem;
+`;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,40 @@
 import { NextPage } from "next";
+import { StyledAbout } from "../components/Styles/Pages/About";
 
 const About: NextPage = () => {
-    return <div>About Page</div>;
+    return (
+        <>
+            <StyledAbout className="container">
+                Arbitraging has always been a way of edging the markets. A
+                profit is always guaranteed when arbitrage is properly done.
+                Whether it&apos;s in sports betting or commodities trading or in
+                currency trading, arbitraging has been practiced for a very long
+                time. With the advent of the internet, it&apos;s even more
+                widespread and it can be practiced with little or no
+                restrictions....but there are still restrictions as brokers and
+                bookmakers have found out ways to identify people practicing
+                arbitrage and suspending their accounts. That&apos;s right, you
+                read that clearly, they suspend your account when they suspect
+                you of arbitraging but will openly welcome you when you lose
+                your money to them. Arbitrage betting while looking easy on
+                paper is not a very easy task to carry out especially manually.
+                The need to register accounts with lots of bookmakers and the
+                timing required to be profitable has served as deterrent to many
+                people who are interested in arbitrage betting. At ArbitrageBet,
+                we have decided to lend a hand, we have decided to bridge this
+                gap by creating a platform where you deposit your funds and make
+                these bets with our up-to-date software with the most precise
+                timing with our accounts. What do we stand to gain? We take a
+                little fraction of the gains while we let you take the rest. In
+                the past, we have shared the games we wagered on and the brokers
+                we used but for security reasons, to prevent bookmakers from
+                tracking down our accounts, we have decided to stop this
+                practice. Are you looking for consistent profits? Simply
+                register with ArbitrageBet and sit back as you enjoy the ride
+                with us!!!
+            </StyledAbout>
+        </>
+    );
 };
 
 export default About;
