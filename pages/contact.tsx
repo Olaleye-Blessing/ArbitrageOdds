a9a432eda6a44aec155c2aadbc12093a589fd485
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-04-07 21:59:49 +0100

    feat: add empty contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { FC } from "react";
+
+const contact: FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Contact | Arbitrageodds</title>
+                <main>Contact Page</main>
+            </Head>
+        </>
+    );
+};
+
+export default contact;
