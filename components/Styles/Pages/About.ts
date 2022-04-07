89621304c2be6cdfee1e20100f64f2c01af881fc
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-04-07 21:47:20 +0100

    update: split long text into paragraphs

--- components/Styles/Pages/About.ts
+++ components/Styles/Pages/About.ts
@@ -2,4 +2,10 @@ import styled from "styled-components";
 
 export const StyledAbout = styled.main`
     padding: 0 2rem 2rem;
+
+    p {
+        &:not(:last-child) {
+            margin-bottom: 2rem;
+        }
+    }
 `;
