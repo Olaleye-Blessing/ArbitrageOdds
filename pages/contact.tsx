572e0057bbc23f7e2ddf1335b5f57ce490700325
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-04-07 22:16:26 +0100

    fix: incorrect tag position

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,13 +1,16 @@
 import Head from "next/head";
 import { FC } from "react";
+import { StyledContact } from "../components/Styles/Pages/Contact";
 
 const contact: FC = () => {
     return (
         <>
             <Head>
                 <title>Contact | Arbitrageodds</title>
-                <main>Contact Page</main>
             </Head>
+            <StyledContact className="container">
+                <h1>Contact Page</h1>
+            </StyledContact>
         </>
     );
 };
